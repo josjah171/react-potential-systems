24490629497507ee2a8a1a4fa536cbfea6593c3e
Josef Jamai-Haitot <josefjamaihaitot@gmail.com>
2019-11-03 17:40:05 +0100

    Updates

---
+++ precache-manifest.c6aec702071a44b7c1a5f5ea9af94771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8129e7dcad068516f9727898a92c5262",
+    "url": "/react-potential-systems/index.html"
+  },
+  {
+    "revision": "bb216ba0b07b7b69b200",
+    "url": "/react-potential-systems/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2c7cff58556eb7426931",
+    "url": "/react-potential-systems/static/js/2.376d0a4b.chunk.js"
+  },
+  {
+    "revision": "bb216ba0b07b7b69b200",
+    "url": "/react-potential-systems/static/js/main.ba1641f5.chunk.js"
+  },
+  {
+    "revision": "d56c6278d966eedc8e4e",
+    "url": "/react-potential-systems/static/js/runtime-main.72e9f2f3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-potential-systems/static/media/logo.25bf045c.svg"
+  }
+]);
